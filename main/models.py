77b805dd0f8a3a4fa88c9bf4af6e8213fb6d8baa
roeef <roeef@mail-wise.com>
2017-11-20 20:43:21 +0200

    Fixed models

--- main/models.py
+++ main/models.py
@@ -6,6 +6,10 @@ from django.db import models
 # Create your models here.
 
 
+class Tags(models.Model):
+    tags = models.CharField(max_length=20)
+
+
 class Mosaic(models.Model):
     title = models.CharField(max_length=200)
     origin = models.CharField(max_length=100)
@@ -34,8 +38,6 @@ class Mosaic(models.Model):
         return self.title
 
 
-class Tags(models.Model):
-    tag = models.CharField(max_length=20)
 
     def __str__(self):
         return self.tag
