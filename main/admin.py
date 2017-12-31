5b9161b9488e0c254818ac77f12c9456e2576502
karinaves <karinaves1991@gmail.com>
2017-12-31 21:23:42 +0200

    added a script for automatic upload - the file is called getData.py + few changes in models + uploaded the excel files needed for the script
    you can uncomment the last line in admin file to make the script run (getData()),
    and also use the following line to run the server:
    manage.py runserver --noreload

--- main/admin.py
+++ main/admin.py
@@ -1,26 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Mosaic, Tags, MosaicPicture
+from main.getData import getData
+from .models import Tag, MosaicPicture, MosaicItem, MosaicSite
 
 
-class PictureInLine (admin.TabularInline):
+class MosaicSiteAdmin(admin.ModelAdmin):
+    list_display = ('site_id', 'title', 'origin')
+    list_display_links = ('site_id', 'title', 'origin')
+
+
+class PictureInline(admin.TabularInline):
     model = MosaicPicture
     extra = 1
     list_display = ('image_tag', 'product',)
     readonly_fields = ('image_tag',)
 
 
-class MosaicAdmin (admin.ModelAdmin):
-    inlines = [PictureInLine]
+class MosaicItemAdmin(admin.ModelAdmin):
+    inlines = [PictureInline]
 
 
-class PictureAdmin (admin.ModelAdmin):
+class PictureAdmin(admin.ModelAdmin):
     fields = ('image_tag',)
     readonly_fields = ('image_tag',)
 
 
-admin.site.register(Tags)
-admin.site.register(Mosaic, MosaicAdmin)
+admin.site.register(Tag)
+admin.site.register(MosaicSite, MosaicSiteAdmin)
+admin.site.register(MosaicItem, MosaicItemAdmin)
 admin.site.register(MosaicPicture, PictureAdmin)
-
+# getData()
