b52140d769561044f39a3ceddb5178844c732a4d
roeef <roeef@mail-wise.com>
2018-01-04 01:29:14 +0200

    import script update in progress

--- main/getData.py
+++ main/getData.py
@@ -9,7 +9,14 @@ import requests
 
 
 def getData():
-    print("hi3")
+    get_photos()
+    xlmosaics = pd.ExcelFile("oglam-mosaics.csv.xlsx").parse("oglam-mosaics.csv")
+    ids = xlmosaics.MISP_RASHUT.unique()
+
+    for id in ids:
+        getMosaic(id, xlmosaics)
+
+def get_photos():
     xlphotos = pd.ExcelFile("listing_of_photos.xlsx").parse("Sheet1")
     photo_names = xlphotos.name.unique()
     print(photo_names)
@@ -17,16 +24,27 @@ def getData():
     # get the connection between photo number and misp_rashut:
     xlcnts = xlcnts.loc[xlcnts['neg_misp'].isin(photo_names)]
     xlmosaics = pd.ExcelFile("oglam-mosaics.csv.xlsx").parse("oglam-mosaics.csv")
-
+    print("Length" + str(len(photo_names)))
+    # to import from filesystem
+    # from os import listdir
+    # from os.path import isfile, join
+    # mypath = os.path.join(settings.BASE_DIR, 'images_to_upload/')
+    # photo_names = ["-".join(f.split("-",2)[:2]).split(".")[0] for f in listdir(mypath) if isfile(join(mypath, f))]
+    i = 0
+    photo_negative_ids = [x.negative_id for x in MosaicPicture.objects.all()]
+    # for photo in photo_names:
     for photo in photo_names:
-        misp_rashut = getMispRashut(photo, xlcnts)
-        if misp_rashut == None:
-            continue
-        mosaic = getMosaic(misp_rashut, xlmosaics)
-        if mosaic == None:
-            print("failed")
-            continue
-        createPhoto(mosaic, photo, xlphotos)
+        if not (photo in photo_negative_ids):
+            misp_rashut = getMispRashut(photo, xlcnts)
+            if misp_rashut == None:
+                print("Error! can't find ", photo)
+                continue
+            mosaic = getMosaic(misp_rashut, xlmosaics)
+            if mosaic == None:
+                print("failed")
+                continue
+            createPhoto(mosaic, photo, xlphotos)
+
 
 def getMispRashut(photo, xlcnts):
     rashut_nums = xlcnts.loc[xlcnts['neg_misp'] == photo]
@@ -51,15 +69,48 @@ def getOrCreateMosaicSite(misp_rashut, xlmosaics):
     new_mosaic_site.site_id = site_id
     new_mosaic_site.title_en = new_mosaic_site.origin_en = mosaic_info.iloc[0]['MOTSA_E'].split('(')[0]
     new_mosaic_site.title_he = new_mosaic_site.origin_he = mosaic_info.iloc[0]['MOTSA'].split('(')[0]
-    new_mosaic_site.period = mosaic_info.iloc[0]['TKU_OBJ_E']
+    new_mosaic_site.period = str(mosaic_info.iloc[0]['TKU_OBJ_E']).lower()
+
     new_mosaic_site.save()
     # TODO: COMPLETE THE MISSING FIELDS FOR MOSAIC SITE
     return new_mosaic_site
 
 
+def getCoef(dimension):
+    if "cm" in dimension.lower() or not "m" in dimension.lower():
+        return 1
+    else:
+        return 100
+    pass
+
+
+def getDimensionsInfo(new_mos, obj_dimensions_str):
+    if obj_dimensions_str == None:
+        return
+    dimensions = str(obj_dimensions_str).split("\n")
+    for dimension in dimensions:
+        nums = [int(s) for s in dimension.split() if s.isdigit()]
+        if len(nums) > 1:
+            print("too many numbers can't parse")
+            return
+        if len(nums) == 0:
+            print("no numbers to parse")
+            return
+        if "length" in dimension.lower():
+            new_mos.length = nums[0] * getCoef(dimension)
+            continue
+        if "width" in dimension.lower():
+            new_mos.width = nums[0] * getCoef(dimension)
+            continue
+        new_mos.area = nums[0]
+    pass
+
+
+
+
 def getMosaic(misp_rashut, xlmosaics):
     mosaic_items = MosaicItem.objects.filter(misp_rashut__startswith = misp_rashut)
-    if len(mosaic_items) > 0:
+    if len(mosaic_items) > 0: # TODO use exist instead of len
         return mosaic_items[0]
 
     #check if a mosaic site exists, if not - create it
@@ -70,20 +121,64 @@ def getMosaic(misp_rashut, xlmosaics):
     # create new mosaic item
     new_mos = MosaicItem()
     new_mos.mosaic_site = mosaic_site
+    new_mos.save()
+    print("saved item")
     new_mos.misp_rashut = misp_rashut
-    new_mos.description = mosaic_info.iloc[0]['OBJ_DESC_E']
-    tagscol = mosaic_info.iloc[0]['TIPUS_E']
-    tagsFromDB = Tag.objects.all()
-    print("not saved", i)
+    new_mos.save()
+    print("saved rashut")
+    new_mos.description_en = mosaic_info.iloc[0]['OBJ_DESC_E']
+    internete = mosaic_info.iloc[0]['INTERNETE']
+    if internete and isinstance(internete, str):
+        new_mos.description_en += ("\n" + internete)
+
+    new_mos.description_he = mosaic_info.iloc[0]['OBJ_DESC']
+    interneth = mosaic_info.iloc[0]['INTERNETH']
+    if interneth and isinstance(interneth,str):
+        new_mos.description_he+=("\n" + interneth)
+
+    getDimensionsInfo(new_mos, mosaic_info.iloc[0]['OBJ_DIM_E'])
+
+    new_mos.rishayon = mosaic_info.iloc[0]['RISHAYON']
+    new_mos.save()
+    print("saved rishayon")
+
+    new_mos.materials = (mosaic_info.iloc[0]['MATERIAL_OBJ_E']).split(",")
+    if isinstance(new_mos.rishayon,str) and '/' in new_mos.rishayon:
+        new_mos.year = (new_mos.rishayon.split('/', 1)[1])
+    if (isinstance(mosaic_info.iloc[0]['BIBE'],str)): #TODO
+        new_mos.bibliography_he = mosaic_info.iloc[0]['BIBE']
+
+    if(isinstance(mosaic_info.iloc[0]['BIBH'],str)): #TODO
+        new_mos.bibliography_en = mosaic_info.iloc[0]['BIBH']
+
+
+    print("not saved33", i)
+
     new_mos.save()
     print("saved", i)
+    tagscol_en = split_if_needed(mosaic_info.iloc[0]['TIPUS_E'])
+    tagscol_he = split_if_needed(mosaic_info.iloc[0]['TIPUS'])
+    # tagscol_en = strip_spaces(tagscol_en) TODO enable after script is running
+    # tagscol_he = strip_spaces(tagscol_he)
 
-    for tag in tagsFromDB:
-        if tag.tag_en in str(tagscol):
-            print(tag.tag_en, tagscol)
-            new_mos.tags.add(tag)
 
+
+
+    tagsFromDB = Tag.objects.all()
+    for idx, tag_from_file in enumerate(tagscol_en):
+        for tag_from_db in tagsFromDB:
+            if tag_from_db.tag_en in str(tag_from_file):
+                print(tag_from_db.tag_en, tag_from_file)
+                new_mos.tags.add(tag_from_db)
+        if not(tagsFromDB.filter(tag_en=tag_from_file).exists()):
+            if (tag_from_file):
+                new_tag = Tag()
+                new_tag.tag_en = tag_from_file
+                new_tag.tag_he = tagscol_he[idx]
+                new_tag.save()
+                new_mos.tags.add(new_tag)
     new_mos.save()
+    print("finshed and saved")
     # TODO: COMPLETE THE MISSING FIELDS FOR MOSAIC ITEM
     return new_mos
 
@@ -92,7 +187,17 @@ def getMosaic(misp_rashut, xlmosaics):
     pass
 
 
-def createPhoto(mosaic, photoname, xlphotos):
+def split_if_needed(string):
+    if isinstance(string,str) and ',' in string:
+        return (string).split(",")
+    else:
+        return [(string)]
+
+def strip_spaces(string):
+    return  [x.strip(' ') for x in string]
+
+
+def     createPhoto(mosaic, photoname, xlphotos):
     photo_row = xlphotos.loc[xlphotos['name'] == photoname]
     if not photo_row.empty:
         link = photo_row.iloc[0]['download_link']
@@ -103,8 +208,7 @@ def createPhoto(mosaic, photoname, xlphotos):
     new_pic.mosaic = mosaic
     new_pic.negative_id = photoname
     filename = os.path.join(
-        settings.BASE_DIR, f'images_to_upload/{photo_full_name}'
-    )
+        settings.BASE_DIR, f'images_to_upload/{photo_full_name}')
     print(photoname, filename)
     try:
         file_object = open(filename, "br")
